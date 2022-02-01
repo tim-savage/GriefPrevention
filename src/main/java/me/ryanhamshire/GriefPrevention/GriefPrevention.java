80f66af633869ed317058eb0ada4c6e85456c613
Tim Savage <tim.savage@gmail.com>
2022-01-31 19:51:38 -0600

    modify player send message methods to accept CommandSender

--- src/main/java/me/ryanhamshire/GriefPrevention/GriefPrevention.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/GriefPrevention.java
@@ -3332,36 +3332,36 @@ public class GriefPrevention extends JavaPlugin
     }
 
     //sends a color-coded message to a player
-    public static void sendMessage(Player player, ChatColor color, Messages messageID, String... args)
+    public static void sendMessage(CommandSender sender, ChatColor color, Messages messageID, String... args)
     {
-        sendMessage(player, color, messageID, 0, args);
+        sendMessage(sender, color, messageID, 0, args);
     }
 
     //sends a color-coded message to a player
-    public static void sendMessage(Player player, ChatColor color, Messages messageID, long delayInTicks, String... args)
+    public static void sendMessage(CommandSender sender, ChatColor color, Messages messageID, long delayInTicks, String... args)
     {
         String message = GriefPrevention.instance.dataStore.getMessage(messageID, args);
-        sendMessage(player, color, message, delayInTicks);
+        sendMessage(sender, color, message, delayInTicks);
     }
 
     //sends a color-coded message to a player
-    public static void sendMessage(Player player, ChatColor color, String message)
+    public static void sendMessage(CommandSender sender, ChatColor color, String message)
     {
         if (message == null || message.length() == 0) return;
 
-        if (player == null)
+        if (sender == null)
         {
             GriefPrevention.AddLogEntry(color + message);
         }
         else
         {
-            player.sendMessage(color + message);
+            sender.sendMessage(color + message);
         }
     }
 
-    public static void sendMessage(Player player, ChatColor color, String message, long delayInTicks)
+    public static void sendMessage(CommandSender sender, ChatColor color, String message, long delayInTicks)
     {
-        SendPlayerMessageTask task = new SendPlayerMessageTask(player, color, message);
+        SendPlayerMessageTask task = new SendPlayerMessageTask(sender, color, message);
 
         //Only schedule if there should be a delay. Otherwise, send the message right now, else the message will appear out of order.
         if (delayInTicks > 0)
