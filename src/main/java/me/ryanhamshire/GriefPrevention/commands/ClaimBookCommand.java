6f788b45abce6a3e9644e6e9eaeae1293f0ed90d
Tim Savage <tim.savage@gmail.com>
2022-02-28 00:49:34 -0600

    migrate claimbook command

---
+++ src/main/java/me/ryanhamshire/GriefPrevention/commands/ClaimBookCommand.java
@@ -0,0 +1,76 @@
+package me.ryanhamshire.GriefPrevention.commands;
+
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import me.ryanhamshire.GriefPrevention.Messages;
+import me.ryanhamshire.GriefPrevention.TextMode;
+import me.ryanhamshire.GriefPrevention.WelcomeTask;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public final class ClaimBookCommand implements CommandExecutor, TabCompleter
+{
+    // reference to plugin main class
+    private final GriefPrevention plugin;
+
+    /**
+     * Class constructor
+     *
+     * @param plugin reference to main class
+     */
+    public ClaimBookCommand(final GriefPrevention plugin)
+    {
+        // set reference to main class
+        this.plugin = plugin;
+
+        // register this class as command executor
+        Objects.requireNonNull(plugin.getCommand("claimbook")).setExecutor(this);
+    }
+
+    @Override
+    public List<String> onTabComplete(@NotNull final CommandSender sender, @NotNull final Command command, @NotNull final String s, @NotNull final String[] args)
+    {
+        if (args.length == 1)
+        {
+            // match first argument with other online players
+            return plugin.commandHandler.matchOnlinePlayers(sender, args[0]);
+        }
+        // if not first argument, return empty list
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command, @NotNull final String s, @NotNull final String[] args)
+    {
+        // command can be issued by player or console
+        Player player = null;
+        if (sender instanceof Player)
+        {
+            player = (Player) sender;
+        }
+
+        //requires one parameter
+        if (args.length != 1) return false;
+
+        //try to find the specified player
+        Player otherPlayer = plugin.getServer().getPlayer(args[0]);
+        if (otherPlayer == null)
+        {
+            GriefPrevention.sendMessage(player, TextMode.Err, Messages.PlayerNotFound2);
+            return true;
+        }
+        else
+        {
+            WelcomeTask task = new WelcomeTask(otherPlayer);
+            task.run();
+            return true;
+        }
+    }
+}
